24127d492d85caf54824b68f0bc20a20d572a270
Marta B-St <mbrst912@gmail.com>
2020-05-06 16:59:08 +0200

    Add Changes

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+
+} else if(randomNumber == 2){
+    computerMove = 'papier';
+
+} else if(randomNumber == 3){
+    computerMove = 'nozyce';
+} 
+
+ printMessage('Mój ruch to: ' + computerMove);
