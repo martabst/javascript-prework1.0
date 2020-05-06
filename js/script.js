fc6c8d717b937bb207e622bde1dcca953ebdd3a3
Marta B-St <mbrst912@gmail.com>
2020-05-06 17:12:50 +0200

    Add Changes

--- js/script.js
+++ js/script.js
@@ -14,4 +14,24 @@ if(randomNumber == 1){
     computerMove = 'nozyce';
 } 
 
- printMessage('Mój ruch to: ' + computerMove);
+printMessage('Mój ruch to: ' + computerMove);
+
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+
+} else if(playerInput == '2'){
+    playerMove = 'papier';
+
+} else if(palyerInput == '3'){
+    playerMove = 'nozyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
